bb37b51b9e17ab8b7a31b65f7771a8e4f61c9564
allworkdone <raj635321@gmail.com>
2022-09-13 18:32:25 +0530

    Shell: Check whether the number is Armstrong number or not In this Program we will take the input Then we do the Armstrong calculation Then we will check if the number is Armstrong number Then we will Print it is or its not an Armstrong Number

---
+++ 05_armstrong_check.sh
@@ -0,0 +1,17 @@
+echo "enter any number "
+read n
+m=$n
+sum=0
+while [ $m -gt 0 ]
+do
+        y=`expr $m % 10`
+        x=`expr $y \* $y \* $y`
+        sum=`expr $sum + $x`
+        m=`expr $m / 10`
+done
+if [ $sum -eq $n ]
+then
+        echo the given number is armstrong
+else
+        echo the given number is not armstrong
+fi
