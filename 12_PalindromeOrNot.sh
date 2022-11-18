09c373901911beed7bf4df26402113321fb50a8a
Abhishek Kumar <76006019+allworkdone@users.noreply.github.com>
2022-11-18 13:07:32 +0530

    Fix! :Shell: Updated code for check Palindrome

--- 12_PalindromeOrNot.sh
+++ 12_PalindromeOrNot.sh
@@ -1,26 +1,15 @@
-#!/bin/bash
+echo "Enter a string : "
+read str
 
-read -p "Enter a number : " num
+len=`expr length $str`
 
-length=$(echo $num | grep -o -E '[0-9]+' | wc -l)
+for (( i=$len-1;i>=0;i-- ))
+do
+	rev="$rev${str:$i:1}"
+done
 
-if [ $length -eq 1 ]; then
-    echo "$num is a Palindrome Number"
-    exit
-fi
-
-len=$(( $length / 2 ))
-
-first=$(echo $num | cut -c 1-$len)
-
-if [ $(( $length % 2 )) -eq 1 ]; then
-    second=$(echo $num | cut -c $(( $len + 2 ))-$length | rev)
+if [ $str = $rev ]; then
+	echo "String is palindrome"
 else
-    second=$(echo $num | cut -c $(( $len + 1 ))-$length | rev)
-fi
-
-if [ $first -eq $second ]; then
-    echo "$num is a Palindrome Number"
-else
-    echo "$num is not a Palindrome Number"
+	echo "String is not a palindrome"
 fi
